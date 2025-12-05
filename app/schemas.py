5b59aac97e90709e93e563aaa45883f2e6aaa35a
Tushar Rajput <tusharo17rajput@gmail.com>
2025-12-05 18:22:58 +0530

    Add user model, schema, and create user endpoint with password hashing

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
+
 
 class PostBase(BaseModel):
     title: str
@@ -11,6 +12,18 @@ class PostCreate(PostBase):
 
 class Post(PostBase):
     id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes= True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
     class Config:
