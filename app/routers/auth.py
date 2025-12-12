bbbeb572a55d4d5eeba25f3fae7bbdd5d0722bf5
Tushar Rajput <tusharo17rajput@gmail.com>
2025-12-12 08:46:32 +0530

    Add JWT authentication with access token creation and verification

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from .. import models, schemas, utils
+from .. import models, utils, oauth2
 from ..database import get_db
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin,db: Session= Depends(get_db)):
-
-  user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm= Depends(),db: Session= Depends(get_db)):
+  # username =
+  # password =
+  user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
   if not user:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
@@ -20,4 +22,6 @@ def login(user_credentials: schemas.UserLogin,db: Session= Depends(get_db)):
   # create token
   # return token
 
-  return {"token": "example token"}
+  access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+  return {"token": access_token, "token_type":"bearer"}
